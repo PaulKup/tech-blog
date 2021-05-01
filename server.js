3a4ad4be83129b6cc52d0843981a46fd0a334f82
ChillyWilly <noreply>
2021-05-01 05:05:38 -0400

    sesson timeout

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 1000*60*60
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
